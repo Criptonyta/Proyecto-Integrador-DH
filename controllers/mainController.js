b20b41fb58aae152103b8d9d6365b27a2876ca72
AlexLangman <alulang@hotmail.com>
2021-09-08 10:30:59 -0300

    cartas de la home corregidas

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,6 +1,11 @@
 const controlador = {
     home: (req, res) => {
-        res.render('home.ejs');
+        let datos = [
+        {userid: 123, artistName: "Jose", img: 'fasdf',skills: 'Guitarrista',bio:'soy un guitarrista'},
+        {userid: 123, artistName: "Alberto", img: 'fasdf',skills: 'Cantante',bio:'soy un cantante'},
+        {userid: 123, artistName: "Carlos", img: 'fasdf',skills: 'Baterista',bio:'soy un baterista'},
+    ]
+        res.render('home.ejs',{datos:datos});
     }
 
 };
