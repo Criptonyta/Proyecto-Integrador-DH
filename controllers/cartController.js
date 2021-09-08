0be5f48732eacaca5401f5003170d4ce5e183540
AlexLangman <alulang@hotmail.com>
2021-09-08 13:00:09 -0300

    Agregando EJS para que se agreguen los paises y provincias con un loop en los carritos

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,10 +1,16 @@
 const controlador = {
     cart1: (req, res) => {
-        res.render('cart1.ejs');
+        let paises = ["","Argentina","Brazil","Chile","EEUU","Mexico","Bolivia","Uruguay","Paraguay","Suecia","Escocia","Inglaterra","EEUU","Mexico","Bolivia","Uruguay","Paraguay","Suecia","Escocia","Inglaterra","Argentina","Brazil","Chile","EEUU","Mexico","Bolivia","Uruguay","Paraguay","Suecia","Escocia","Inglaterra","EEUU","Mexico","Bolivia","Uruguay","Paraguay","Suecia","Escocia","Inglaterra"]
+        let provincias = ["","Buenos Aires", "CABA","Cordoba","Mendoza"]
+
+        res.render('cart1.ejs',{paises:paises,provincias:provincias});
     },
 
     cart2: (req, res) => {
-        res.render('cart2.ejs');
+        let paises = ["Paises","Argentina","Brazil","Chile","EEUU","Mexico","Bolivia","Uruguay","Paraguay","Suecia","Escocia","Inglaterra","EEUU","Mexico","Bolivia","Uruguay","Paraguay","Suecia","Escocia","Inglaterra","Argentina","Brazil","Chile","EEUU","Mexico","Bolivia","Uruguay","Paraguay","Suecia","Escocia","Inglaterra","EEUU","Mexico","Bolivia","Uruguay","Paraguay","Suecia","Escocia","Inglaterra"]
+        let provincias = ["Provincia","Buenos Aires", "CABA","Cordoba","Mendoza"]
+
+        res.render('cart2.ejs',{paises:paises,provincias:provincias});
     },
 
     cart3: (req, res) => {
