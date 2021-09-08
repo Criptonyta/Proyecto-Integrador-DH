a3c65f4c619afd69bafcb54408f5fcbcb2dcdc76
AlexLangman <alulang@hotmail.com>
2021-09-08 11:56:08 -0300

    agregando las cartas de canciones e instrumentos de la tienda

--- controllers/productController.js
+++ controllers/productController.js
@@ -12,13 +12,29 @@ const controlador = {
     },
 
     tienda: (req, res) => {
+        let musicos = [
+            {img:"tapadisco1.jpg",tituloCancion:"Titulo de la cancion",artistaCancion:"Nombre de artista"},
+            {img:"tapadisco2.jpg",tituloCancion:"Titulo de la cancion",artistaCancion:"Nombre de artista"},
+            {img:"tapadisco3.jpg",tituloCancion:"Titulo de la cancion",artistaCancion:"Nombre de artista"},
+            {img:"tapadisco4.jpg",tituloCancion:"Titulo de la cancion",artistaCancion:"Nombre de artista"},
+            {img:"tapadisco5.jpg",tituloCancion:"Titulo de la cancion",artistaCancion:"Nombre de artista"},
+            {img:"tapadisco6.jpg",tituloCancion:"Titulo de la cancion",artistaCancion:"Nombre de artista"},
+        ]
+        let instrumentos = [
+            {img:"bass.jpg",tituloInstrumento:"Titulo de la instrumento",precio:"2000$"},
+            {img:"bass.jpg",tituloInstrumento:"Titulo de la instrumento",precio:"2000$"},
+            {img:"bass.jpg",tituloInstrumento:"Titulo de la instrumento",precio:"2000$"},
+            {img:"bass.jpg",tituloInstrumento:"Titulo de la instrumento",precio:"2000$"},
+            {img:"bass.jpg",tituloInstrumento:"Titulo de la instrumento",precio:"2000$"},
+            {img:"bass.jpg",tituloInstrumento:"Titulo de la instrumento",precio:"2000$"},
+        ]
         let datos = [
             {userid: 123, artistName: "Jose", img: 'fasdf',skills: 'Guitarrista',bio:'soy un guitarrista'},
             {userid: 123, artistName: "Alberto", img: 'fasdf',skills: 'Cantante',bio:'soy un cantante'},
             {userid: 123, artistName: "Carlos", img: 'fasdf',skills: 'Baterista',bio:'soy un baterista'},
             {userid: 123, artistName: "Carlos", img: 'fasdf',skills: 'Baterista',bio:'soy un baterista'},
         ]
-        res.render('tienda.ejs',{datos:datos});
+        res.render('tienda.ejs',{datos:datos,musicos:musicos,instrumentos:instrumentos});
     }
 };
 
