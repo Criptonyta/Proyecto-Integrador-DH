06547f9def59d994ae5e81e9e91ee238bb22a03a
AlexLangman <alulang@hotmail.com>
2021-09-08 14:31:59 -0300

    Agregando las fotos con EJS en la tienda y en la home

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,9 +1,9 @@
 const controlador = {
     home: (req, res) => {
         let datos = [
-        {userid: 123, artistName: "Jose", img: 'fasdf',skills: 'Guitarrista',bio:'soy un guitarrista'},
-        {userid: 123, artistName: "Alberto", img: 'fasdf',skills: 'Cantante',bio:'soy un cantante'},
-        {userid: 123, artistName: "Carlos", img: 'fasdf',skills: 'Baterista',bio:'soy un baterista'},
+        {userid: 123, artistName: "Jose", img: 'recomendado1Cropped.JPG',skills: 'Guitarrista',bio:'soy un guitarrista'},
+        {userid: 123, artistName: "Alberto", img: 'recomendado2Cropped.JPG',skills: 'Cantante',bio:'soy un cantante'},
+        {userid: 123, artistName: "Carlos", img: 'recomendado3Cropped.JPG',skills: 'Baterista',bio:'soy un baterista'},
     ]
         res.render('home.ejs',{datos:datos});
     }
--- controllers/productController.js
+++ controllers/productController.js
@@ -22,17 +22,17 @@ const controlador = {
         ]
         let instrumentos = [
             {img:"bass.jpg",tituloInstrumento:"Titulo de la instrumento",precio:"2000$"},
-            {img:"bass.jpg",tituloInstrumento:"Titulo de la instrumento",precio:"2000$"},
-            {img:"bass.jpg",tituloInstrumento:"Titulo de la instrumento",precio:"2000$"},
-            {img:"bass.jpg",tituloInstrumento:"Titulo de la instrumento",precio:"2000$"},
-            {img:"bass.jpg",tituloInstrumento:"Titulo de la instrumento",precio:"2000$"},
-            {img:"bass.jpg",tituloInstrumento:"Titulo de la instrumento",precio:"2000$"},
+            {img:"instrum_piano_fondoblanco.jpg",tituloInstrumento:"Titulo de la instrumento",precio:"2000$"},
+            {img:"instrum_saxo_fondoblanco.jpg",tituloInstrumento:"Titulo de la instrumento",precio:"2000$"},
+            {img:"instrum_bateria_fondoblanco.jpg",tituloInstrumento:"Titulo de la instrumento",precio:"2000$"},
+            {img:"instrum_guitarra_fondoblanco.jpg",tituloInstrumento:"Titulo de la instrumento",precio:"2000$"},
+            {img:"instrum_teclado_fondoblanco.jpg",tituloInstrumento:"Titulo de la instrumento",precio:"2000$"},
         ]
         let datos = [
-            {userid: 123, artistName: "Jose", img: 'fasdf',skills: 'Guitarrista',bio:'soy un guitarrista'},
-            {userid: 123, artistName: "Alberto", img: 'fasdf',skills: 'Cantante',bio:'soy un cantante'},
-            {userid: 123, artistName: "Carlos", img: 'fasdf',skills: 'Baterista',bio:'soy un baterista'},
-            {userid: 123, artistName: "Carlos", img: 'fasdf',skills: 'Baterista',bio:'soy un baterista'},
+            {userid: 123, artistName: "Jose", img: 'recomendado1Cropped.JPG',skills: 'Guitarrista',bio:'soy un guitarrista'},
+            {userid: 123, artistName: "Alberto", img: 'recomendado2Cropped.JPG',skills: 'Cantante',bio:'soy un cantante'},
+            {userid: 123, artistName: "Carlos", img: 'recomendado3Cropped.JPG',skills: 'Baterista',bio:'soy un baterista'},
+            {userid: 123, artistName: "Carlos", img: 'recomendado2Cropped.JPG',skills: 'Baterista',bio:'soy un baterista'},
         ]
         res.render('tienda.ejs',{datos:datos,musicos:musicos,instrumentos:instrumentos});
     }
