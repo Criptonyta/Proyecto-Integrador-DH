fb4c93d8dd545e797e50f9e0cde95e89f303570f
AlexLangman <alulang@hotmail.com>
2021-09-14 22:29:18 -0300

    Carta de la home conectada a la base de datos JSON

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,10 +1,14 @@
+const usersDB = require('../database/usersDB.json'); 
+
 const controlador = {
     home: (req, res) => {
-        let datos = [
+        /*let datos = [
         {userid: 123, artistName: "Jose", img: 'recomendado1Cropped.JPG',skills: 'Guitarrista',bio:'soy un guitarrista'},
         {userid: 123, artistName: "Alberto", img: 'recomendado2Cropped.JPG',skills: 'Cantante',bio:'soy un cantante'},
         {userid: 123, artistName: "Carlos", img: 'recomendado3Cropped.JPG',skills: 'Baterista',bio:'soy un baterista'},
-    ]
+    ]*/
+
+        const datos = usersDB.slice(1,4)
         res.render('home.ejs',{datos:datos});
     }
 
