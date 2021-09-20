b49e1a0fd123b397ea95adc5dba028e89c638669
Criptonyta <daninavaza@gmail.com>
2021-09-20 20:51:57 -0300

    Mod: Tratando de resolver el deploy en HEroku

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,33 +1,42 @@
-const fs = require ('fs');
-const path = require ("path");
+const fs = require('fs');
+const path = require("path");
 const pathInstruments = path.join(__dirname, '../database/instrumentsDB.json');
 const pathSongs = path.join(__dirname, '../database/songsDB.json');
-const pathUsers= path.join(__dirname, '../database/usersDB.json');
-const auxiliares = require("../public/funcionesAuxiliares/ProductControllerAux.js")
- 
+const pathUsers = path.join(__dirname, '../database/usersDB.json');
+const pathFuncionesAuxiliares = path.join(__dirname, '../public/funcionesAuxiliares/productControllerAux.js');
+
+
 const controlador = {
     instrumentDetail: (req, res) => {
         const instrumentos = JSON.parse(fs.readFileSync(pathInstruments));
         const instrumento = instrumentos.find(elemento => elemento.InstrumId == req.params.id);
-        const relacionados = instrumentos.slice(1,5)//Instrumentos relacionados
+        const relacionados = instrumentos.slice(1, 5) //Instrumentos relacionados
 
-        res.render('product.ejs', {producto:instrumento,relacionados});
+        res.render('product.ejs', {
+            producto: instrumento,
+            relacionados
+        });
 
     },
     songDetail: (req, res) => {
         const songs = JSON.parse(fs.readFileSync(pathSongs));
         const song = songs.find(elemento => elemento.songId == req.params.id);
-        const relacionados = songs.slice(1,5)//Instrumentos relacionados
-        res.render('product.ejs', {producto:song,relacionados});
+        const relacionados = songs.slice(1, 5) //Instrumentos relacionados
+        res.render('product.ejs', {
+            producto: song,
+            relacionados
+        });
 
     },
-    artistDetail:(req, res) => {//FALTA HACER
+    artistDetail: (req, res) => { //FALTA HACER
 
     },
     //falta hacer la vista de usuario
     userDetail: (req, res) => {
         const songs = JSON.parse(fsreadFileSync(pathUsers));
-        res.render('product.ejs', {user});
+        res.render('product.ejs', {
+            user
+        });
     },
 
     productempty: (req, res) => {
@@ -37,79 +46,100 @@ const controlador = {
     songempty: (req, res) => {
         res.render('songempty.ejs');
     },
-    
+
     addSong: (req, res) => {
-        const fs = require ('fs');
+        const fs = require('fs');
         const songsDB = JSON.parse(fs.readFileSync(pathSongs, "UTF-8"));
         songcargar = {
-            id:25, /* Id usuario */
-            songId:1, /* Id canción */
-            img:req.body.img,
-            audioFile:req.body.song,
-            titulo:req.body.titulo,
-            precio:req.body.precio,
-            descripcion:req.body.descripcion,
-    }
-    songsDB.push(songcargar)
-    fs.writeFileSync(pathSongs, JSON.stringify(songsDB))
-    const instrumentsDB = JSON.parse(fs.readFileSync(pathInstruments));
-    const usersDB =JSON.parse(fs.readFileSync(pathUsers));
+            id: 25,
+            /* Id usuario */
+            songId: 1,
+            /* Id canción */
+            img: req.body.img,
+            audioFile: req.body.song,
+            titulo: req.body.titulo,
+            precio: req.body.precio,
+            descripcion: req.body.descripcion,
+        }
+        songsDB.push(songcargar)
+        fs.writeFileSync(pathSongs, JSON.stringify(songsDB))
+        const instrumentsDB = JSON.parse(fs.readFileSync(pathInstruments));
+        const usersDB = JSON.parse(fs.readFileSync(pathUsers));
 
-    const instrumentos = instrumentsDB.slice(1,7);        
-    const musicos = songsDB.slice(1,7);
+        const instrumentos = instrumentsDB.slice(1, 7);
+        const musicos = songsDB.slice(1, 7);
 
-    const artistsDB = usersDB.filter(item => item.bio != "")//Los artistas son los que tienen bio
-    const datos = artistsDB.slice(1,5);
-   
-    res.render('tienda.ejs',{datos:datos,musicos:musicos,instrumentos:instrumentos})
+        const artistsDB = usersDB.filter(item => item.bio != "") //Los artistas son los que tienen bio
+        const datos = artistsDB.slice(1, 5);
 
-      
+        res.render('tienda.ejs', {
+            datos: datos,
+            musicos: musicos,
+            instrumentos: instrumentos
+        })
+
+
 
     },
 
     tienda: (req, res) => {
         const instrumentsDB = JSON.parse(fs.readFileSync(pathInstruments));
         const songsDB = JSON.parse(fs.readFileSync(pathSongs));
-        const usersDB =JSON.parse(fs.readFileSync(pathUsers));
+        const usersDB = JSON.parse(fs.readFileSync(pathUsers));
 
-        const instrumentos = instrumentsDB.slice(1,7);        
-        const musicos = songsDB.slice(1,7);
+        const instrumentos = instrumentsDB.slice(1, 7);
+        const musicos = songsDB.slice(1, 7);
 
-        const artistsDB = usersDB.filter(item => item.bio != "")//Los artistas son los que tienen bio
-        const datos = artistsDB.slice(1,5);
-       
-        res.render('tienda.ejs',{datos:datos,musicos:musicos,instrumentos:instrumentos});
+        const artistsDB = usersDB.filter(item => item.bio != "") //Los artistas son los que tienen bio
+        const datos = artistsDB.slice(1, 5);
+
+        res.render('tienda.ejs', {
+            datos: datos,
+            musicos: musicos,
+            instrumentos: instrumentos
+        });
     },
-    songs: (req,res) => {
+    songs: (req, res) => {
         const songsDB = JSON.parse(fs.readFileSync(pathSongs));
         const musicos = songsDB;
-        res.render("allSongs.ejs",{musicos})
+        res.render("allSongs.ejs", {
+            musicos
+        })
     },
-    instruments:(req,res) => {
+    instruments: (req, res) => {
         const instrumentsDB = JSON.parse(fs.readFileSync(pathInstruments));
         const instrumentos = instrumentsDB;
-        res.render("allInstruments.ejs",{instrumentos})
+        res.render("allInstruments.ejs", {
+            instrumentos
+        })
     },
-    artists:(req,res) => {
+    artists: (req, res) => {
         const usersDB = JSON.parse(fs.readFileSync(pathUsers));
-        const artistsDB = usersDB.filter(item => item.bio != "")//Los artistas son los que tienen bio
+        const artistsDB = usersDB.filter(item => item.bio != "") //Los artistas son los que tienen bio
         const artistas = artistsDB;
-        res.render("allArtists.ejs",{artistas})
+        res.render("allArtists.ejs", {
+            artistas
+        })
     },
-    searched:(req,res) => {//Controlador para mostrar cuando se busca algo
+    searched: (req, res) => { //Controlador para mostrar cuando se busca algo
         const instrumentsDB = JSON.parse(fs.readFileSync(pathInstruments));
         const songsDB = JSON.parse(fs.readFileSync(pathSongs));
-        const usersDB =JSON.parse(fs.readFileSync(pathUsers));
-        const artistsDB = usersDB.filter(item => item.bio != "")//Los artistas son los que tienen bio
+        const usersDB = JSON.parse(fs.readFileSync(pathUsers));
+        const artistsDB = usersDB.filter(item => item.bio != "") //Los artistas son los que tienen bio
+        const auxiliares = require(pathFuncionesAuxiliares);
 
         const buscado = req.query.search
         const palabras = buscado.split(' ')
-        const resultadosSongs = auxiliares.checkAtribute(songsDB,["titulo","descripcion","nombre","apellido"],palabras)//Canciones que coinciden
-        const resultadosInstruments = auxiliares.checkAtribute(instrumentsDB,["titulo","descripcion"],palabras)//Instrumentos que coinciden
-        const resultadosArtistas = auxiliares.checkAtribute(artistsDB,["nombre","apellido","skills","bio"],palabras)//Artistas que coinciden
+        const resultadosSongs = auxiliares.checkAtribute(songsDB, ["titulo", "descripcion", "nombre", "apellido"], palabras) //Canciones que coinciden
+        const resultadosInstruments = auxiliares.checkAtribute(instrumentsDB, ["titulo", "descripcion"], palabras) //Instrumentos que coinciden
+        const resultadosArtistas = auxiliares.checkAtribute(artistsDB, ["nombre", "apellido", "skills", "bio"], palabras) //Artistas que coinciden
 
 
-        res.render("allSearched.ejs",{musicos:resultadosSongs,instrumentos:resultadosInstruments,artistas:resultadosArtistas})
+        res.render("allSearched.ejs", {
+            musicos: resultadosSongs,
+            instrumentos: resultadosInstruments,
+            artistas: resultadosArtistas
+        })
     }
 };
 
--- public/funcionesAuxiliares/productControllerAux.js
+++ public/funcionesAuxiliares/productControllerAux.js
@@ -1,25 +1,27 @@
 //Funcion que le pasas una DB, los atributos de la DB (en forma de lista) y las palabras buscadas (en forma de lista) 
 //y te devuelve los elementos que cumplen en forma de lista
-function checkAtribute(DB,atributos,palabrasBuscadas){
+function checkAtribute(DB, atributos, palabrasBuscadas) {
     let resultado = DB.filter(item => {
-        let palabrasItemTotal = [];//Todas las palabras en donde voy a buscar
-        for (let i=0; i< atributos.length; i++) {
+        let palabrasItemTotal = []; //Todas las palabras en donde voy a buscar
+        for (let i = 0; i < atributos.length; i++) {
             palabrasItem = item[atributos[i]].split(" ")
-            for (let j=0; j< palabrasItem.length; j++){
-                palabrasItemTotal.push(palabrasItem[j])
+            for (let j = 0; j < palabrasItem.length; j++) {
+                palabrasItemTotal.push(palabrasItem[j].toLowerCase())
             }
         }
         let contador = 0; //Si el contador vale lo mismo que palabrasItem, todas las palabras se encontraron
-        for (let i=0; i< palabrasItemTotal.length; i++) {
-            if (palabrasBuscadas.includes(palabrasItemTotal[i])){
+        for (let i = 0; i < palabrasItemTotal.length; i++) {
+            if (palabrasBuscadas.includes(palabrasItemTotal[i].toLowerCase())) {
                 contador += 1
             }
         }
-        if (contador == palabrasBuscadas.length){
+        if (contador == palabrasBuscadas.length) {
             return item
         }
     })
     return resultado
-}
+};
 
-module.exports = {checkAtribute}
+module.exports = {
+    checkAtribute
+};
