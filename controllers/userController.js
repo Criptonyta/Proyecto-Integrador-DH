fa11bb31496f537b7344c04a75dc6c20cbb2bcfe
AlexLangman <alulang@hotmail.com>
2021-09-23 15:52:52 -0300

    agregando el put para edicion de usuarios

--- controllers/userController.js
+++ controllers/userController.js
@@ -32,16 +32,40 @@ const controlador = {
 
         res.render('userprofileEdit.ejs',{anuncioUser,usuarioInfo,skills});
     },
-    userprofileEditNew: (req, res) => {
+    userprofileEditNew: (req, res) => {//editar usuario
         const userDB = require(pathUsers);
         const songsDB = require(pathSongs);
         const instrumentsDB = require(pathInstruments);
 
         const skills = ["Bajista","Baterista","Cantante","Guitarrista","Multiinstrumentalista","Productor","Otros"]
 
-        res.send(req.body)
+        const profileNew = {email:req.body.email,
+                           nombre:req.body.nombre,
+                           apellido:req.body.apellido,
+                           password:req.body.password,
+                           userAvatarButton:req.body.userAvatarButton,
+                           minibio:req.body.minibio,
+                           skills:req.body.skills}
+
+        userDB.forEach(function(usuario){
+            if (usuario.id == req.params.iduser){
+                usuario.email = profileNew.email;
+                usuario.nombre = profileNew.nombre;
+                usuario.apellido = profileNew.apellido;
+                usuario.password = profileNew.password;
+                usuario.minibio = profileNew.minibio;
+                usuario.skills = profileNew.skills;
+            }
+        })
+        fs.writeFileSync(pathUsers,JSON.stringify(userDB))
+
+        const usuarioInfo = userDB.find(usuario => usuario.id == req.params.iduser)
+        const songsUser = songsDB.filter(song => song.id == req.params.iduser);
+        const instrumentUser = instrumentsDB.filter(instrument => instrument.id == req.params.iduser);
+        const anuncioUser = [...songsUser, ...instrumentUser];
 
 
+        res.render('userprofile.ejs', {anuncioUser,usuarioInfo,songsUser,instrumentUser});
 
     },
 
