dc1f3ccd95cb816c31658523c807eeb05929299b
AlexLangman <alulang@hotmail.com>
2021-11-04 19:42:47 -0300

    agregando detalles a la navbar

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -2,11 +2,16 @@ window.addEventListener("load",function(){
     let burgerButton = document.querySelector("#navbar-burgerButton")
     let desplegable = document.querySelector(".navbar-desplegable")
     burgerButton.addEventListener("click",function(){
-        if (desplegable.style.width == "0%"){
-            desplegable.style.width = "50%"
+        if (desplegable.style.opacity == "0"){
+            desplegable.style.opacity = "100%"
         }
         else {
-            desplegable.style.width = "0%"
+            desplegable.style.opacity = "0%"
+        }
+    })
+    window.addEventListener("resize",function(){
+        if (window.innerWidth >= "700"){
+            desplegable.style.opacity = "0%"
         }
     })
 })
