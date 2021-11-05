bfac76675e6729a6b5fec87ee92d873f38097035
AlexLangman <alulang@hotmail.com>
2021-11-05 16:00:05 -0300

    corigiendo detallles de la navbar2

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,6 +1,8 @@
 window.addEventListener("load", function () {
     let burgerButton = document.querySelector("#navbar-burgerButton")
     let desplegable = document.querySelector(".navbar-desplegable")
+    let iconoTienda = document.querySelector("#navbar-desplegable-tienda")
+    let acordion = document.querySelector(".acordion-vertical")
     burgerButton.addEventListener("click", function () {
         if (desplegable.style.opacity == "0") {
             desplegable.style.opacity = "100%"
@@ -12,5 +14,13 @@ window.addEventListener("load", function () {
         if (window.innerWidth >= "700") {
             desplegable.style.opacity = "0%"
         }
+    })
+    iconoTienda.addEventListener("click",function(){
+      if (acordion.style.height > "0px"){
+          acordion.style.height = "0px"
+      }
+      else {
+        acordion.style.height = "auto"
+      }
     })
 })
