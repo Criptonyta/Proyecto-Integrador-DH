ba9fc3fd4ef533898b46cbcda95cb95ac6738bcd
Criptonyta <daninavaza@gmail.com>
2021-11-11 19:09:29 -0300

    Add: Asincronismo en MainController.js

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,26 +1,35 @@
-const fs = require ('fs');
-const path = require ("path");
+const fs = require('fs');
+const path = require("path");
 
-const {usersModel} = require("../models/index")//Importamos los models
+const {
+    usersModel
+} = require("../models/index") //Importamos los models
 const pathFuncionesAuxiliares = path.join(__dirname, '../public/funcionesAuxiliares/mainControllerAux.js');
 const auxiliares = require(pathFuncionesAuxiliares);
 
 const controlador = {
-    home: (req, res) => {
-        const artistsDB = usersModel.findArtists()//Los artistas son los que tienen bio
-        const datos = auxiliares.buscarNelementosAleatorios(artistsDB,"id",3);
+    home: async (req, res) => {
+        try {
+            const artistsDB = await usersModel.findArtists() //Los artistas son los que tienen bio
+            const datos = await auxiliares.buscarNelementosAleatorios(artistsDB, "id", 3);
 
-        //Creamos la variable locals para usar en la vista
-        if (req.session.userLogged ==  undefined){res.locals.idusuario = "noLogueado"}
-        else if (req.session !=  undefined){
-            res.locals.idusuario = req.session.userLogged.id;
-            res.locals.nombre = req.session.userLogged.nombre;
+            //Creamos la variable locals para usar en la vista
+            if (req.session.userLogged == undefined) {
+                res.locals.idusuario = "noLogueado"
+            } else if (req.session != undefined) {
+                res.locals.idusuario = req.session.userLogged.id;
+                res.locals.nombre = req.session.userLogged.nombre;
+            } else {
+                res.locals.idusuario = req.cookie.recordame.id
+            }
+
+            res.render('home.ejs', {
+                datos: datos
+            });
+        } catch (e) {
+            console.log('error buscando y presentando los artistas')
         }
-        else{res.locals.idusuario = req.cookie.recordame.id}
-
-        res.render('home.ejs',{datos:datos});
     }
-
 };
 
 
