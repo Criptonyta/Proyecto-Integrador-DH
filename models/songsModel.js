e50d777cbc46f23fe5ebf4f0e2c92df9b0c4d19c
Criptonyta <daninavaza@gmail.com>
2021-11-19 23:46:36 -0300

    Add: Resuelto y agregado el dump datos+estructura de musiqueirosdb4

--- models/songsModel.js
+++ models/songsModel.js
@@ -21,8 +21,10 @@ const songsModel = {
     },
     findSong: async function (idsong) { //Busca cancion por ID
         try {
-            let songs = await this.getAll()
-            let song = songs.find(item => item.songId == idsong)
+            let song = await db.songsdb.findByPk(idsong)
+
+            // let songs = await this.getAll()
+            // let song = songs.find(item => item.songId == idsong)
             return song
         } catch (error) {
             console.log("Error tratando de encontrar la cancion: " + error)
