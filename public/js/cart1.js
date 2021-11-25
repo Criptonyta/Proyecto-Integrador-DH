b36351029e31c3a494d347b3f589e4553de6e1ec
AlexLangman <alulang@hotmail.com>
2021-11-25 13:04:39 -0300

    funcionalidad del carrito para el boton vaciar carrito

--- public/js/cart1.js
+++ public/js/cart1.js
@@ -2,11 +2,20 @@ window.addEventListener("load",function(){
     let carritoContainer = document.getElementById("cart1Alex-tablaDatosContainer")
     let tableContainer = document.getElementById("cart1Alex-tablaDatos")
     let productosCarrito = JSON.parse(sessionStorage.getItem("productoCarrito"))
+    let borrarTodo = document.getElementById("carrito-BorrarTodo")
+    let actualizarDatos = document.getElementById("carrito-actualizarDatos")
+    let irATienda = document.getElementById("cart1Alex-botonesOpcionSeguir")
     //FALTA PRECIO Y DESCRIPCION QUE HAY QUE TRAERLOS CON API
-    if (productosCarrito.length == 0){
+    if (!productosCarrito){
         tableContainer.style.display = "none"
+        actualizarDatos.style.display = "none"
+        borrarTodo.style.display = "none"
+        irATienda.style.display = "none"
     }
     else {
+        actualizarDatos.style.display = "block"
+        borrarTodo.style.display = "block"
+        irATienda.style.display = "block"
         let rtaColumna = 
         `<table id="cart1Alex-tablaDatos">
             <tr>
@@ -19,7 +28,7 @@ window.addEventListener("load",function(){
         </table>`
         tableContainer.innerHTML += rtaColumna
         for (let i = 0; i < productosCarrito.length; i++) {
-            if (productosCarrito[i].tipoProducto == "instrum"){
+            if (productosCarrito[i].tipoProducto == "instrum"){//Si es intrummento
                 //let producto = fetch(apiURL)
                 let rtaFila = 
                 `<tr>
@@ -31,7 +40,7 @@ window.addEventListener("load",function(){
                 </tr>`
                 tableContainer.innerHTML+=rtaFila
             }
-            else if (productosCarrito[i].tipoProducto == "cancion"){
+            else if (productosCarrito[i].tipoProducto == "cancion"){//Si es cancion
                 //let producto = fetch(apiURL)
                 let rtaFila = 
                 `<tr>
@@ -45,4 +54,10 @@ window.addEventListener("load",function(){
             }
         }
     }
+    borrarTodo.addEventListener("click",function(){
+        if (confirm("Esta seguro que desea vaciar el carrito?")){
+            sessionStorage.removeItem("productoCarrito")
+            location.reload()
+        }
+    })
 })
