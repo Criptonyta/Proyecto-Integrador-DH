ce27abc3af711a607c1cdbc383fddcdcdd6a0415
AlexLangman <alulang@hotmail.com>
2021-11-25 15:34:07 -0300

    agregando funcionalidad a los botnes + y - (no salieron todavia)

--- public/js/cart1.js
+++ public/js/cart1.js
@@ -1,10 +1,35 @@
+function Sumar(id,tipoProducto){//Funcion para sumar 1 a cantidad (id y tipoProducto)
+    let productosCarro = JSON.parse(sessionStorage.getItem("productoCarrito"))
+    for (let i = 0; i < productosCarrito.length; i++) {
+        if (productosCarro[i].id == id && productosCarro[i].tipoProducto == tipoProducto.toString()){
+            productosCarro.cantidad++
+        }
+        sessionStorage.setItem("productoCarrito",JSON.stringify(productosCarro))
+        location.reload()
+    }
+}
+function Restar(id,tipoProducto){//Funcion para restar 1 a cantidad
+    let productosCarro = JSON.parse(sessionStorage.getItem("productoCarrito"))
+    for (let i = 0; i < productosCarrito.length; i++) {
+        if (productosCarro[i].id == id && productosCarro[i].tipoProducto == tipoProducto){
+            productosCarro.cantidad--
+        }
+        sessionStorage.setItem("productoCarrito",JSON.stringify(productosCarro))
+        location.reload()
+    }
+}
+function Saludar(param1,param2){
+    console.log("hola")
+}
+
 window.addEventListener("load",function(){
-    let carritoContainer = document.getElementById("cart1Alex-tablaDatosContainer")
     let tableContainer = document.getElementById("cart1Alex-tablaDatos")
     let productosCarrito = JSON.parse(sessionStorage.getItem("productoCarrito"))
     let borrarTodo = document.getElementById("carrito-BorrarTodo")
     let actualizarDatos = document.getElementById("carrito-actualizarDatos")
     let irATienda = document.getElementById("cart1Alex-botonesOpcionSeguir")
+    let botonSumar = document.querySelector(".fa-plus")
+    let botonRestar = document.querySelector(".fa-minus")
     //FALTA PRECIO Y DESCRIPCION QUE HAY QUE TRAERLOS CON API
     if (!productosCarrito){
         tableContainer.style.display = "none"
@@ -22,7 +47,7 @@ window.addEventListener("load",function(){
                 <th>Producto</th>
                 <th>Descripcion del producto</th>
                 <th>Precio unitario</th>
-                <th>Cantidad</th>
+                <th>Cantidad</i></i></th>
                 <th>Total</th>
             </tr>
         </table>`
@@ -35,7 +60,13 @@ window.addEventListener("load",function(){
                     <td><a href="/products/detailInstrument/${productosCarrito[i].id}">FALTA API</a></td>
                     <td>FALTA API</td>
                     <td>FALTA API</td>
-                    <td>${productosCarrito[i].cantidad}</td>
+                    <td>
+                        <div>
+                            <span>${productosCarrito[i].cantidad}</span>
+                            <span onclick="Sumar(1,'instrum')"><i class="fas fa-plus"></i></span>
+                            <span><i class="fas fa-minus"></i></span>
+                        </div>
+                    </td>
                     <td>FALTA API (cuenta)</td>
                 </tr>`
                 tableContainer.innerHTML+=rtaFila
@@ -47,7 +78,7 @@ window.addEventListener("load",function(){
                     <td><a href="/products/detailSong/${productosCarrito[i].id}">FALTA API</a></td>
                     <td>FALTA API</td>
                     <td>FALTA API</td>
-                    <td>${productosCarrito[i].cantidad}</td>
+                    <td>${productosCarrito[i].cantidad}<i class="fas fa-plus"><i class="fas fa-minus"></td>
                     <td>FALTA API (cuenta)</td>
                 </tr>`
                 tableContainer.innerHTML+=rtaFila
