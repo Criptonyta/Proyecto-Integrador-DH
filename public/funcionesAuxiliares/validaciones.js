ec451ee40b39ca36b1a604a5069ba91d72648a33
Agostina Pavese <agostinap85@gmail.com>
2021-11-27 20:26:20 -0300

    Validaciones en Login finalizadas

--- public/funcionesAuxiliares/validaciones.js
+++ public/funcionesAuxiliares/validaciones.js
@@ -5,69 +5,121 @@
 // Contraseña(Obligatoria.Deberá tener al menos 8 caracteres. (Opcional) → Deberá tener letras mayúsculas, minúsculas, un número y un carácter especial.
 // Imagen (Deberá ser un archivo válido (JPG, JPEG, PNG, GIF))
 
-window.addEventListener("load", function validaciones() {
-   let formulario = document.querySelector('.form-register'); // Atrapo el formulario con la clase reserva
+window.addEventListener("load", function validacionesRegistro() {
+   let registroForm = document.querySelector('.form-register'); // Atrapo el formulario con la clase reserva
 
-    formulario.addEventListener('submit', function (e) {
+    registroForm.addEventListener('submit', function (e) {
 
-        let errores = []; // Creo un array de errores para ir guardando los resultados y mostrarlos en el html
+        let erroresRegister = []; // Creo un array de errores para ir guardando los resultados y mostrarlos en el html
 
         //Inicio las validaciones, capturando primero cada campo del form
         let campoNombre = document.querySelector("#nombre");
 
         if (campoNombre.value == "") { // Si el valor de campoNombre es vacio, entonces...
-           console.log("El campo de nombre debe estar completo") // Que mande el error a la variable
+           erroresRegister.push("El campo de nombre debe estar completo") // Que mande el error a la variable
         } else if (campoNombre.value.length < 2 ) { // Si ingreso menos de 2 caracteres, entonces....
-            errores.push("El campo nombre debe tener al menos 2 caracteres")
+            erroresRegister.push("El campo nombre debe tener al menos 2 caracteres")
         }
 
         let campoApellido = document.querySelector("#apellido");
 
         if (campoApellido.value == "") { // Si el valor de campoApellido es vacio, entonces...
-            errores.push("El campo apellido debe estar completo") // Que mande el error a la variable
+            erroresRegister.push("El campo apellido debe estar completo") // Que mande el error a la variable
         } else if (campoApellido.value.length < 2 ) { // Si ingreso menos de 2 caracteres, entonces....
-            errores.push("El campo apellido debe tener al menos 2 caracteres")
+            erroresRegister.push("El campo apellido debe tener al menos 2 caracteres")
         }
 
          let campoEmail = document.querySelector("#email");
          let expresionRegular =  /\S+@\S+.\S+/;
 
         if (expresionRegular.test (campoEmail)) { // Si el valor de campoEmail no contiene elementos de la expresión regular, entonces...
-             errores.push("El campo email debe ser válido") // Que mande el error a la variable
+             erroresRegister.push("El campo email debe ser válido") // Que mande el error a la variable
          } else if  (campoEmail.value == "") { // Si el campo está vacío, entonces....
-            errores.push("El campo email es obligatorio")
+            erroresRegister.push("El campo email es obligatorio")
          }
         
          let campoContrasena = document.querySelector("#password");
 
          if (campoContrasena.value == "") { // Si el valor de campoContraseña es vacio, entonces...
-             errores.push("El campo contraseña debe estar completo") // Que mande el error a la variable
+             erroresRegister.push("El campo contraseña debe estar completo") // Que mande el error a la variable
          } else if (campoContrasena.value.length < 8 ) { // Si ingreso menos de 8 caracteres, entonces....
-            errores.push("El campo contraseña debe tener al menos 8 caracteres")
+            erroresRegister.push("El campo contraseña debe tener al menos 8 caracteres")
          }
 
          let campoImagen = document.querySelector("#userAvatarRegisterButton"); // <--FALTA COMPLETAR!
 
          if (campoImagen.value == "") { // Si el valor de campoImagen es vacio, entonces...
-            errores.push("El campo imagen debe estar completo") // Que mande el error a la variable
+            erroresRegister.push("El campo imagen debe estar completo") // Que mande el error a la variable
          } else if (campoApellido.value.length < 2 ) { // Si ingreso menos de 2 caracteres, entonces....
-             errores.push("El campo apellido debe tener al menos 2 caracteres")
+             erroresRegister.push("El campo apellido debe tener al menos 2 caracteres")
          }
 
-        
+         if (erroresRegister.length > 0) {
+            e.preventDefault(); //Prevengo el comportamiento por default del evento submit solo si hay error
+
+            let ulErrores = document.querySelector('validator-errors')
+            for (let i = 0; i < erroresRegister.length; i++) { // recorre el array de errores
+                // al ulErrores, le modifico el contenido y agrego la descripcion de cada error encontrado
+                ulErrores.innerHTML += "<li>" + erroresRegister[i] + "</li>"
+            }
+
+         }
     })              
-})    
+})  
 
-module.exports = {validaciones}
+
     
-
-
-
 /*
 // VALIDACIONES DE LOGIN DE USUARIOS
-//Email (Obligatorio. deberá ser válido.(Opcional) → Debe existir en la base.)
-//Contraseña (Obligatoria.)
-// Creación y modificación de productos (Nombre Obligatorio. Deberá tener al menos 5 caracteres)
+//Email (Obligatorio. deberá ser válido.(Opcional → Debe existir en la base))
+//Contraseña (Obligatoria)
+*/
+
+window.addEventListener("load", function validacionesLogin() {
+
+    let loginForm = document.querySelector('#loginForm');
+
+    loginForm.addEventListener('submit', function (e) {
+        
+        let erroresLogin = []; // Creo un array de errores para ir guardando los resultados y mostrarlos en el html
+        
+        let campoEmail = document.querySelector("#emailLogin");
+         let expresionRegular =  /\S+@\S+.\S+/;
+
+        if (expresionRegular.test (campoEmail)) { // Si el valor de campoEmail no contiene elementos de la expresión regular, entonces...
+            erroresLogin.push("El campo email debe ser válido") // Que mande el error a la variable
+         } else if  (campoEmail.value == "") { // Si el campo está vacío, entonces....
+            erroresLogin.push("El campo email es obligatorio")
+         }
+        
+         let campoContrasena = document.querySelector("#passwordLogin");
+
+         if (campoContrasena.value == "") { // Si el valor de campoContraseña es vacio, entonces...
+            erroresLogin.push("El campo contraseña debe estar completo") // Que mande el error a la variable
+         } else if (campoContrasena.value.length < 8 ) { // Si ingreso menos de 8 caracteres, entonces....
+            erroresLogin.push("El campo contraseña debe tener al menos 8 caracteres")
+         }
+
+         if (erroresLogin.length > 0) {
+            e.preventDefault(); //Prevengo el comportamiento por default del evento submit solo si hay error
+
+            let ulErrores = document.querySelector('validator-errors')
+            for (let i = 0; i < erroresLogin.length; i++) { // recorre el array de errores
+                // al ulErrores, le modifico el contenido y agrego la descripcion de cada error encontrado
+                ulErrores.innerHTML += "<li>" + erroresLogin[i] + "</li>"
+            }
+
+         }
+      
+         
+    })
+
+})
+
+// CREACION Y MODIFICACION DE PRODUCTOS
+// (Nombre Obligatorio. Deberá tener al menos 5 caracteres)
 // Descripción (Deberá tener al menos 20 caracteres)
 //Imagen(Deberá ser un archivo válido (JPG, JPEG, PNG, GIF))
-*/
+
+
+module.exports = {validacionesRegistro, validacionesLogin}
