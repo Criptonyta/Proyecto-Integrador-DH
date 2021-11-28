54055e03bb87362b918ecf0753c297751e241feb
AlexLangman <alulang@hotmail.com>
2021-11-28 14:58:27 -0300

    agregando el preview de la foto para editar cancion

---
+++ public/js/editSong.js
@@ -0,0 +1,28 @@
+function showPreview(event,id){
+    if (event.target.files.length>0 && event.target.files[0].type == "image/jpeg"){
+        let src = URL.createObjectURL(event.target.files[0])
+        let preview = document.getElementById(id)
+        preview.src = src
+    }
+    else {
+        let foto =  document.getElementById("editSong-coverPhoto")
+        let inputFoto = document.getElementById("songEmptyImgBtn")
+        foto.style.display = "none"
+        inputFoto.value = null
+        alert("Solo se pueden cargar fotos!")
+    }
+}
+
+window.addEventListener("load",function(){
+    let foto = document.getElementById("editSong-coverPhoto")
+    let inputFoto = document.getElementById("songEmptyImgBtn")
+    inputFoto.addEventListener("change",function(e){
+        try {
+            foto.style.display = "block"
+            showPreview(e,"editSong-coverPhoto")
+        } catch (error) {
+            foto.style.display = "none"
+            console.log(error)
+        }
+    })
+})
