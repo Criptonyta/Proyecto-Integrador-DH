f51edacd4f36343c94fc52b0a3eeff543e8830c3
Agostina Pavese <agostinap85@gmail.com>
2021-11-27 21:19:33 -0300

    Validaciones Productos y Canciones finalizadas, falta imagen

--- public/funcionesAuxiliares/validaciones.js
+++ public/funcionesAuxiliares/validaciones.js
@@ -51,7 +51,7 @@ window.addEventListener("load", function validacionesRegistro() {
          if (campoImagen.value == "") { // Si el valor de campoImagen es vacio, entonces...
             erroresRegister.push("El campo imagen debe estar completo") // Que mande el error a la variable
          } else if (campoApellido.value.length < 2 ) { // Si ingreso menos de 2 caracteres, entonces....
-             erroresRegister.push("El campo apellido debe tener al menos 2 caracteres")
+             erroresRegister.push("El campo imagen debe tener al menos 2 caracteres")
          }
 
          if (erroresRegister.length > 0) {
@@ -116,10 +116,103 @@ window.addEventListener("load", function validacionesLogin() {
 
 })
 
-// CREACION Y MODIFICACION DE PRODUCTOS
+// CREACION Y MODIFICACION DE PRODUCTOS / TRABAJA SOBRE PRODUCTEMPTY.EJS
 // (Nombre Obligatorio. Deberá tener al menos 5 caracteres)
 // Descripción (Deberá tener al menos 20 caracteres)
 //Imagen(Deberá ser un archivo válido (JPG, JPEG, PNG, GIF))
 
+window.addEventListener("load", function validacionesProductos() {
+    let editProductForm = document.querySelector('#product-container'); // Atrapo el formulario con la clase reserva
+ 
+    editProductForm.addEventListener('submit', function (e) {
+ 
+         let erroresProducts = []; // Creo un array de errores para ir guardando los resultados y mostrarlos en el html
+ 
+         //Inicio las validaciones, capturando primero cada campo del form
+         let campoNombre = document.querySelector("#titulo");
+ 
+         if (campoTitulo.value == "") { // Si el valor de campoNombre es vacio, entonces...
+            erroresProducts.push("El campo título debe estar completo") // Que mande el error a la variable
+         } else if (campoTitulo.value.length < 5 ) { // Si ingreso menos de 5 caracteres, entonces....
+            erroresProducts.push("El campo título debe tener al menos 5 caracteres")
+         }
+ 
+         let campoDescripcion = document.querySelector("#descripcion");
+ 
+         if (campoDescripcion.value == "") { // Si el valor de campoDescripcion es vacio, entonces...
+            erroresProducts.push("El campo descripción debe estar completo") // Que mande el error a la variable
+         } else if (campoDescripcion.value.length < 20 ) { // Si ingreso menos de 20 caracteres, entonces....
+            erroresProducts.push("El campo descripción debe tener al menos 20 caracteres")
+         }
 
-module.exports = {validacionesRegistro, validacionesLogin}
+         let campoImagen = document.querySelector("#productEmptyButton"); // <--FALTA COMPLETAR!
+
+         if (campoImagen.value == "") { // Si el valor de campoImagen es vacio, entonces...
+            erroresProducts.push("El campo imagen debe estar completo") // Que mande el error a la variable
+         } else if (campoImagen.value.length < 2 ) { // Si ingreso menos de 2 caracteres, entonces....
+             erroresProducts.push("El campo imagen debe tener al menos 2 caracteres")
+         }
+
+         if (erroresProducts.length > 0) {
+            e.preventDefault(); //Prevengo el comportamiento por default del evento submit solo si hay error
+
+            let ulErrores = document.querySelector('validator-errors')
+            for (let i = 0; i < erroresProducts.length; i++) { // recorre el array de errores
+                // al ulErrores, le modifico el contenido y agrego la descripcion de cada error encontrado
+                ulErrores.innerHTML += "<li>" + erroresProducts[i] + "</li>"
+            }
+        }        
+    })
+})
+
+// CREACION Y MODIFICACION DE CANCIONES / TRABAJA SOBRE SONGEMPTY.EJS
+// (Nombre Obligatorio. Deberá tener al menos 5 caracteres)
+// Descripción (Deberá tener al menos 20 caracteres)
+//Imagen(Deberá ser un archivo válido (JPG, JPEG, PNG, GIF))
+
+window.addEventListener("load", function validacionesSongs() {
+    let editSongForm = document.querySelector('#song-container'); // Atrapo el formulario con la clase reserva
+ 
+    editSongForm.addEventListener('submit', function (e) {
+ 
+         let erroresSongs = []; // Creo un array de errores para ir guardando los resultados y mostrarlos en el html
+ 
+         //Inicio las validaciones, capturando primero cada campo del form
+         let campoTitulo = document.querySelector("#titulo");
+ 
+         if (campoTitulo.value == "") { // Si el valor de campoTitulo es vacio, entonces...
+            erroresSongs.push("El campo título debe estar completo") // Que mande el error a la variable
+         } else if (campoTitulo.value.length < 5 ) { // Si ingreso menos de 5 caracteres, entonces....
+            erroresSongs.push("El campo título debe tener al menos 5 caracteres")
+         }
+ 
+         let campoDescripcion = document.querySelector("#descripcion");
+ 
+         if (campoDescripcion.value == "") { // Si el valor de campoDescripcion es vacio, entonces...
+            erroresSongs.push("El campo descripción debe estar completo") // Que mande el error a la variable
+         } else if (campoDescripcion.value.length < 20 ) { // Si ingreso menos de 20 caracteres, entonces....
+            erroresSongs.push("El campo descripción debe tener al menos 20 caracteres")
+         }
+
+         let campoImagen = document.querySelector("#productEmptyButton"); // <--FALTA COMPLETAR!
+
+         if (campoImagen.value == "") { // Si el valor de campoImagen es vacio, entonces...
+            erroresSongs.push("El campo imagen debe estar completo") // Que mande el error a la variable
+         } else if (campoImagen.value.length < 2 ) { // Si ingreso menos de 2 caracteres, entonces....
+            erroresSongs.push("El campo imagen debe tener al menos 2 caracteres")
+         }
+
+         if (erroresSongs.length > 0) {
+            e.preventDefault(); //Prevengo el comportamiento por default del evento submit solo si hay error
+
+            let ulErrores = document.querySelector('validator-errors')
+            for (let i = 0; i < erroresSongs.length; i++) { // recorre el array de errores
+                // al ulErrores, le modifico el contenido y agrego la descripcion de cada error encontrado
+                ulErrores.innerHTML += "<li>" + erroresSongs[i] + "</li>"
+            }
+        }        
+    })
+})
+
+
+module.exports = {validacionesRegistro, validacionesLogin, validacionesProductos, validacionesSongs}
